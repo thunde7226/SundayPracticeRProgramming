42eec286818392309f186ac6431e2a58b527082f
thunde7226 <thunde7226cp@gmail.com>
2022-08-28 09:28:38 -0400

    First commit for SundayR_Practice run

---
+++ SundayR_Aug28.R
@@ -0,0 +1,12 @@
+##Practicing with git this is August 28 2022 ##
+
+install.packages("skimr")
+summary(iris)
+install.packages("janitor")
+library(janitor)
+install.packages("dplyr")
+library(dplyr)
+install.packages("palmerpenguins")
+library(palmerpenguins)
+skim_without_charts(penguins)
+glimpse(penguins)
